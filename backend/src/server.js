1f6030b8c6e0a34f9cf7e3cbb4b2b44c895960e3
Al-Bazazi <sshaheerabbasid@gmail.com>
2025-08-06 15:19:59 +0500

    Changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,25 +10,14 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const __dirname = path.resolve();
 
-if(process.env.NODE_ENV !== "production") {
-    app.use(cors({
-        origin: "http://localhost:5173"
-    }));
-    app.use(express.json());
-    app.use(rateLimiter);
-}
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
+app.use(express.json());
+app.use(rateLimiter);
 
 app.use("/notes",notesRoutes);
-
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
-    app.get("*",(req,res) => {
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-})
-}
-
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("App Listening on PORT: ",PORT);
