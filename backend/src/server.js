478287329418e3a546e7cbdaecdf322f34af7b7d
Al-Bazazi <sshaheerabbasid@gmail.com>
2025-08-06 15:42:47 +0500

    Fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-
+import path from "path";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
@@ -10,14 +9,17 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
 app.use(express.json());
 app.use(rateLimiter);
-
 app.use("/notes",notesRoutes);
+
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+app.get("*",(res,req) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+});
+
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("App Listening on PORT: ",PORT);
