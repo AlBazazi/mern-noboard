a71693b799ebf6ce60be7870d353dffb6d1df369
AlBazazi <168605550+AlBazazi@users.noreply.github.com>
2025-08-09 16:24:46 +0500

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,13 +9,23 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+if(process.env.NODE_ENV !== "production") {
 app.use(cors({
       origin: "http://127.0.0.1:5173",
     })
 );
+}
 app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes",notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
