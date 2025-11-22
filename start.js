14ffb12301b441d120c1075dd7b6ae8a6c5bb1d5
AlBazazi <168605550+AlBazazi@users.noreply.github.com>
2025-11-22 22:29:18 +0500

    Delete start.js

--- start.js
+++
@@ -1,3 +0,0 @@
-import { spawn } from "child_process";
-const backend = spawn("npm", ["run", "start", "--prefix", "backend"], { stdio: "inherit" });
-const frontend = spawn("npm", ["run", "dev", "--prefix", "frontend"], { stdio: "inherit" });
